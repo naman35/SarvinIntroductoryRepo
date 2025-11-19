2b9ab895c77138e3a04408706042ddc917ff12ff
Naman Agarwal <agarwalnaman35@gmail.com>
2025-11-19 15:02:46 +0530

    root

---
+++ app/root.jsx
@@ -0,0 +1,30 @@
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+export const meta: MetaFunction = () => {
+  return [{ title: "My Remix App" }];
+};
+
+export default function Root() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
