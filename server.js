c0da347cdee3c28e8d7a79068bd551b2bc45a2e2
Naman Agarwal <agarwalnaman35@gmail.com>
2025-11-19 23:31:53 +0530

    change

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
-import { createRequestHandler } from "@vercel/remix";
-import * as build from "./build/server/index.js";
+import { createRequestHandler } from "@remix-run/node"; // Adjust the import based on your Remix version
+import * as build from "./build/index.js"; // Instead of build/server/index.js
 
-export default createRequestHandler({ build });
+export default function handler(req, res) {
+  return createRequestHandler({
+    build,
+    getLoadContext() {
+      // Provide your load context (optional)
+      return {};
+    },
+  })(req, res);
+}
